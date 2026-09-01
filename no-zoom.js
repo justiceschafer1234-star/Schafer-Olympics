a8205378401456ff44b11317b79178de2772e6d7
justiceschafer1234-star <justice.schafer1234@gmail.com>
2026-09-01 09:00:52 -0400

    Load integrated Game Day event controls on HQ

--- no-zoom.js
+++ no-zoom.js
@@ -8,4 +8,11 @@
   document.addEventListener('touchmove', (event) => {
     if (event.touches && event.touches.length > 1) event.preventDefault();
   }, { passive: false });
+
+  if (document.querySelector('#events-grid')) {
+    const script = document.createElement('script');
+    script.src = '/gameday-events.js';
+    script.defer = true;
+    document.head.appendChild(script);
+  }
 })();
