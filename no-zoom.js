50e116270a66ff406b93aa0a28215d0bb06a06ae
justiceschafer1234-star <justice.schafer1234@gmail.com>
2026-09-02 12:43:53 -0400

    Load team rosters on standalone team events

--- no-zoom.js
+++ no-zoom.js
@@ -15,4 +15,12 @@
     script.defer = true;
     document.head.appendChild(script);
   }
+
+  const teamEventPaths=new Set(['/adult-soccer.html','/cornhole-tournament.html','/wiffle-ball-tournament.html','/kids-slip-and-slide.html','/adult-slip-and-slide.html','/egg-toss.html','/kids-soccer.html','/junior-basketball.html']);
+  if(teamEventPaths.has(location.pathname.replace(/\/+$/,''))){
+    const script=document.createElement('script');
+    script.src='/team-event-rosters.js?v=1';
+    script.defer=true;
+    document.head.appendChild(script);
+  }
 })();
