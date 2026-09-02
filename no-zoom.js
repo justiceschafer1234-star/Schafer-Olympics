473c685d11061a6a60ab296d637b5e1ccc52635b
justiceschafer1234-star <justice.schafer1234@gmail.com>
2026-09-02 12:56:35 -0400

    Load Copper medal UI on standalone events

--- no-zoom.js
+++ no-zoom.js
@@ -18,9 +18,15 @@
 
   const teamEventPaths=new Set(['/adult-soccer.html','/cornhole-tournament.html','/wiffle-ball-tournament.html','/kids-slip-and-slide.html','/adult-slip-and-slide.html','/egg-toss.html','/kids-soccer.html','/junior-basketball.html']);
   if(teamEventPaths.has(location.pathname.replace(/\/+$/,''))){
-    const script=document.createElement('script');
-    script.src='/team-event-rosters.js?v=1';
-    script.defer=true;
-    document.head.appendChild(script);
+    const rosterScript=document.createElement('script');
+    rosterScript.src='/team-event-rosters.js?v=1';
+    rosterScript.defer=true;
+    document.head.appendChild(rosterScript);
+    if(!document.querySelector('script[src*="copper-medal-ui.js"]')){
+      const copperScript=document.createElement('script');
+      copperScript.src='/copper-medal-ui.js?v=1';
+      copperScript.defer=true;
+      document.head.appendChild(copperScript);
+    }
   }
 })();
