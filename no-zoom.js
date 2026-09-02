47180ae846a3c330c969204d8412244e1c022310
justiceschafer1234-star <justice.schafer1234@gmail.com>
2026-09-02 15:32:54 -0400

    Load shared team color bars across event pages

--- no-zoom.js
+++ no-zoom.js
@@ -16,8 +16,20 @@
     document.head.appendChild(script);
   }
 
-  const teamEventPaths=new Set(['/adult-soccer.html','/cornhole-tournament.html','/wiffle-ball-tournament.html','/kids-slip-and-slide.html','/adult-slip-and-slide.html','/egg-toss.html','/kids-soccer.html','/junior-basketball.html']);
-  if(teamEventPaths.has(location.pathname.replace(/\/+$/,''))){
+  const teamEventPaths=new Set([
+    '/adult-soccer','/adult-soccer.html',
+    '/cornhole-tournament','/cornhole-tournament.html',
+    '/wiffle-ball-tournament','/wiffle-ball-tournament.html',
+    '/kids-slip-and-slide','/kids-slip-and-slide.html',
+    '/adult-slip-and-slide','/adult-slip-and-slide.html',
+    '/egg-toss','/egg-toss.html',
+    '/kids-soccer','/kids-soccer.html',
+    '/junior-basketball','/junior-basketball.html',
+    '/speed-grab','/speed-grab.html',
+    '/event-scorecard','/event-scorecard.html'
+  ]);
+  const path=location.pathname.replace(/\/+$/,'')||'/';
+  if(teamEventPaths.has(path)){
     const rosterScript=document.createElement('script');
     rosterScript.src='/team-event-rosters.js?v=1';
     rosterScript.defer=true;
@@ -27,6 +39,12 @@
       copperScript.src='/copper-medal-ui.js?v=1';
       copperScript.defer=true;
       document.head.appendChild(copperScript);
+    }
+    if(!document.querySelector('script[src*="team-color-bars.js"]')){
+      const bars=document.createElement('script');
+      bars.src='/team-color-bars.js?v=20260902-1';
+      bars.defer=true;
+      document.head.appendChild(bars);
     }
   }
 })();
